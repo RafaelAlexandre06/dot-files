640af6464c0b8bda25535c8cb530407cc3fb6f5a
RafaelAlexandre06 <fc62223@alunos.fc.ul.pt>
2025-10-07 02:53:49 +0100

    clangd and language servers lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,43 +1,34 @@
--- lsp.lua
-if not vim.lsp or not vim.lsp.configs then
-  return  -- nvim-lspconfig not loaded yet
-end
+-- ~/.config/nvim/lua/lsp.lua
+-- Future-proof LSP setup for Neovim >= 0.11
 
-local lspconfigs = vim.lsp.configs
+-- Require lspconfig plugin
+local lspconfig = require("lspconfig")
+local util = lspconfig.util
 
--- C / C++
-if not lspconfigs.clangd then
-  lspconfigs.clangd = {
-    default_config = {
-      cmd = { "clangd" },
-      filetypes = { "c", "cpp", "objc", "objcpp" },
-      root_dir = vim.fs.dirname,
-      settings = {},
-    }
-  }
-end
-vim.lsp.start({ name = "clangd" })
+-- === C/C++ (clangd) ===
+lspconfig.clangd.setup({
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = util.root_pattern("compile_commands.json", ".git", "."),
+})
 
--- Java
-if not lspconfigs.jdtls then
-  lspconfigs.jdtls = {
-    default_config = {
-      cmd = { "jdtls" },
-      root_dir = vim.fs.dirname,
-      settings = {},
+-- === Java (jdtls) ===
+-- Make sure jdtls is installed and in your PATH
+lspconfig.jdtls.setup({
+    cmd = { "jdtls" },
+    filetypes = { "java" },
+    root_dir = util.root_pattern(".git", "pom.xml", "build.gradle"),
+})
+
+-- === Haskell (hls) ===
+-- Make sure haskell-language-server is installed
+lspconfig.hls.setup{
+    cmd = { "/home/rafa/.ghcup/bin/haskell-language-server-wrapper", "--lsp" },
+    filetypes = { "haskell", "lhaskell" },
+    root_dir = lspconfig.util.root_pattern("*.cabal", "stack.yaml", "cabal.project", "hie.yaml"),
+    settings = {
+        haskell = {
+            formattingProvider = "ormolu", -- or "brittany"
+        }
     }
-  }
-end
-vim.lsp.start({ name = "jdtls" })
--- Haskell
-if not lspconfigs.hls then
-  lspconfigs.hls = {
-    default_config = {
-      cmd = { "haskell-language-server-wrapper", "--lsp" },
-      filetypes = { "haskell", "lhaskell" },
-      root_dir = vim.fs.dirname,
-      settings = {},
-    }
-  }
-end
-vim.lsp.start({ name = "hls" })
+}
